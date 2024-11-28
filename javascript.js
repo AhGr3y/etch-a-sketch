02eae1abe4523f3e6799479149f42d1eb89caa5c
ahgr3y <leehh97@gmail.com>
2024-11-28 16:38:42 +0800

    Add grid to draw and a reset button

--- javascript.js
+++ javascript.js
@@ -1,2 +1,52 @@
-const div = document.querySelector("div");
-div.textContent = "Hello world!";
+const containerLength = 650;
+const container = document.querySelector("#container");
+container.style.height = `${containerLength}px`;
+container.style.width = `${containerLength}px`;
+container.style.marginTop = "20px";
+
+generateGrid(16);
+
+const resetBtn = document.querySelector("#reset-button");
+resetBtn.style.backgroundColor = "white";
+resetBtn.style.marginTop = "20px";
+resetBtn.style.padding = "3px 15px";
+resetBtn.style.borderRadius = "7px";
+
+resetBtn.addEventListener("mouseenter", () => {
+    resetBtn.style.backgroundColor = "grey";
+});
+
+resetBtn.addEventListener("mouseleave", () => {
+    resetBtn.style.backgroundColor = "white";
+});
+
+resetBtn.addEventListener("click", function(event) {
+    const userChoice = prompt("How many you want?");
+    if (userChoice && (+userChoice <= 100 && +userChoice > 0)) {
+        removeChildren(container);
+        generateGrid(+userChoice);
+    }
+});
+
+function generateGrid(squaresPerSide) {
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        const square = document.createElement("div");
+        square.style.width = `${Math.floor(containerLength / squaresPerSide)}px`;
+        square.style.height = `${Math.floor(containerLength / squaresPerSide)}px`;
+        square.style.border = "1px solid black";
+        square.style.margin = 0;
+        square.style.padding = 0;
+        square.addEventListener("mouseover", function(event) {
+            square.style.backgroundColor = "grey";
+        });
+        container.appendChild(square);
+    }
+}
+
+function removeChildren(elem) {
+    let child = elem.lastElementChild;
+    while (child) {
+        elem.removeChild(child);
+        child = elem.lastElementChild;
+    }
+}
